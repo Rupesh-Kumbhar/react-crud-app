6a11eff97785349f57c3303dffd7178c462e14a2
Rupesh Kumbhar <rp.kumbhar2001@gmail.com>
2024-03-25 11:40:08 +0530

    first commit rk branch,dev branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          from rk to dev branch
         </a>
       </header>
     </div>
